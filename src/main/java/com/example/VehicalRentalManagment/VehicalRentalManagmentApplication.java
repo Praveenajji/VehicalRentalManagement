b6abe2ca4d433a1a262aaaaecf7bf90692a985f2
Praveenajji <praveengovind2.0@gmail.com>
2024-12-10 22:00:17 +0530

    projectcreated

---
+++ src/main/java/com/example/VehicalRentalManagment/VehicalRentalManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.VehicalRentalManagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicalRentalManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicalRentalManagmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/VehicalRentalManagment/VehicalRentalManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.VehicalRentalManagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicalRentalManagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
